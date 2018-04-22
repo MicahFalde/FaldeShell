513c80789add3acb9a36b1e6b84084f48f124ab8
lickwid <micahfalde@gmail.com>
2018-04-22 16:58:09 -0400

    Add files via upload

---
+++ 100 Random File PS Script.ps1
@@ -0,0 +1,28 @@
+﻿#variable declaration
+
+$page = Invoke-WebRequest https://www.zyxware.com/articles/4344/list-of-fortune-500-companies-and-their-websites
+
+$links = $page.links
+
+$tables = @($page.ParsedHtml.getElementsByTagName("TABLE"))
+
+$tableRows = $tables[0].rows
+
+
+
+#loops through the table to get only the top 100 urls.
+
+$urlArray = @()
+
+foreach ($tablerow in $tablerows){
+        
+        $urlArray += New-Object psobject -Property @{'URLName' = $tablerow.innerHTML.Split('"')[1]}
+        
+        #write-host ($tablerow.innerHTML).Split('"')[1]
+
+        $i++
+
+        if($i -eq 101){Break}
+}
+
+
